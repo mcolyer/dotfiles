f02d8dfe5da7f18869f026489b061e12ebd4fa21
Matt Colyer <mcolyer@github.com>
2016-10-03 12:37:19 -0700

    Add vim plugins

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd ~/
 
 ln -s ~/.dotfiles/gvimrc.after ~/.gvimrc.after
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/vim-plugins ~/.vim-plugins
+ln -s ~/.dotfiles/vim-plugins ~/.janus
 ln -s ~/.dotfiles/vimrc.after ~/.vimrc.after
 ln -s ~/.dotfiles/vimrc.before ~/.vimrc.before
 ln -s ~/.dotfiles/gemrc ~/.gemrc
