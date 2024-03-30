aa57c011c314567e6a0ea1e087119dd2e135a8a9
Matt Colyer <matt@colyer.name>
2024-03-30 23:32:44 +0000

    Fixes for install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,8 @@ set -x
 cd ~/
 
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/nvim/
 ln -s ~/.dotfiles/init.vim ~/.config/nvim/init.vim
+# Install plugged
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
